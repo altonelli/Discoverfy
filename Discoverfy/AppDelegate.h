f4849411f1fa80127369b82c6c77e58c4bfff7a2
altonelli <altonelli14@gmail.com>
2016-07-26 11:20:52 -0700

    Initial Commit

---
+++ Discoverfy/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Discoverfy
+//
+//  Created by Arthur Tonelli on 7/26/16.
+//  Copyright © 2016 Arthur Tonelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
